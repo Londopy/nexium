01e9657ce73acc1272cf6146aaaf8995338b9fd3
Londopy <Londopy@users.noreply.github.com>
2026-09-19 12:21:05 -0700

    IntTy::min and max no longer overflow for i128

--- src/types.rs
+++ src/types.rs
@@ -35,14 +35,18 @@ impl IntTy {
         }
     }
     pub fn min(self) -> i128 {
-        if self.is_signed() {
+        if self == IntTy::I128 {
+            i128::MIN
+        } else if self.is_signed() {
             -(1i128 << (self.bits() - 1))
         } else {
             0
         }
     }
     pub fn max(self) -> i128 {
-        if self.is_signed() {
+        if self == IntTy::I128 {
+            i128::MAX
+        } else if self.is_signed() {
             (1i128 << (self.bits() - 1)) - 1
         } else if self.bits() == 128 {
             i128::MAX
