ee2c210f855f35703ed0f7ae0acc0bbe09b939e3
Londopy <Londopy@users.noreply.github.com>
2026-09-24 07:12:59 +0000

    packaging: the manifests for v1.3.0

--- installers/chocolatey/tools/chocolateyinstall.ps1
+++ installers/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
     packageName    = 'nexium'
     fileType       = 'exe'
-    url64bit       = 'https://github.com/Londopy/nexium/releases/download/v1.2.1/nexium-1.2.1-setup-x64.exe'
-    checksum64     = 'd759a51ccef292939dedee88a34836b501302492947c6f63158ba5e1e1567f3f'
+    url64bit       = 'https://github.com/Londopy/nexium/releases/download/v1.3.0/nexium-1.3.0-setup-x64.exe'
+    checksum64     = 'd82a3a151ac40865dddcf34f16b2b4d191a88522a287df95c6b91b72113095e3'
     checksumType64 = 'sha256'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /TASKS=addtopath'
     validExitCodes = @(0)
